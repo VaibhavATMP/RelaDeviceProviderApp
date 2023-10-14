56dfd71adcda82b576379ebe30860480af338334
vaibhav <vaibhav.hadpe@archents.com>
2023-10-14 14:57:41 +0530

    3rd line added through master

--- git1/src/test/java/package1/P1Class1.java
+++ git1/src/test/java/package1/P1Class1.java
@@ -10,5 +10,8 @@ public class P1Class1
         System.out.println("1.Package 1A Class1A");
         System.out.println("2.Package 1A Class1A");
         System.out.println("2nd line added in P1Class1");
+
+        System.out.println("3.Package 1A Class1A");
+        System.out.println("3rd line added in P1Class1");
     }
 }
--- git1/src/test/java/package2/P2Class2.java
+++ git1/src/test/java/package2/P2Class2.java
@@ -7,5 +7,8 @@ public class P2Class2
         System.out.println("1.Package 2A Class2A");
         System.out.println("2.Package 2A Class2A");
         System.out.println("2nd line added P2Class2");
+
+        System.out.println("3.Package 2A Class2A");
+        System.out.println("3rd line added P2Class2");
     }
 }
