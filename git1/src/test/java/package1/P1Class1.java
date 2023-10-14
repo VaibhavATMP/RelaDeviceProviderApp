e501adbe70c80be041b31616850cd378ad18d2f6
vaibhav <vaibhav.hadpe@archents.com>
2023-10-14 14:23:57 +0530

    Project Structure Created clone it

---
+++ git1/src/test/java/package1/P1Class1.java
@@ -0,0 +1,11 @@
+package package1;
+
+
+
+public class P1Class1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Package 1A Class1A");
+    }
+}
---
+++ git1/src/test/java/package2/P2Class2.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class P2Class2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1.Package 2A Class2A");
+    }
+}
