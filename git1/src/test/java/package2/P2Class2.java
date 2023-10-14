c1211486d6bdae227faccc09fb9cf44d22ac5549
vaibhav <vaibhav.hadpe@archents.com>
2023-10-14 14:42:27 +0530

    Added 2nd line

--- git1/src/test/java/package2/P2Class2.java
+++ git1/src/test/java/package2/P2Class2.java
@@ -5,5 +5,7 @@ public class P2Class2
     public static void main(String[] args)
     {
         System.out.println("1.Package 2A Class2A");
+        System.out.println("2.Package 2A Class2A");
+        System.out.println("2nd line added P2Class2");
     }
 }
